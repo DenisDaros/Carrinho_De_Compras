9542b5f27ecb0d076d603dc34b2ceed95d3af6e7
Denis <denisv.daros@gmail.com>
2022-06-09 19:24:27 -0300

    terceiro requisito concluido

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (id) => {
+ try {
+  const link = `https://api.mercadolibre.com/items/${id}`;
+  const retorno = await fetch(link);
+  const tratando = await retorno.json();
+  return tratando;
+} catch (error) {
+   return error;
+}
 };
 
 if (typeof module !== 'undefined') {
